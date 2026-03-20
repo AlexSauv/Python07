85b789733107568abb58155d5b4a7bbcc498151c
AlexSauv <141260346+AlexSauv@users.noreply.github.com>
2026-03-20 23:24:38 +0100

    adding Artefact basics

--- ex1/ArtifactCard.py
+++ ex1/ArtifactCard.py
@@ -1,1 +1,24 @@
 from ex0.Card import Card
+
+
+class ArtifactCard(Card):
+    def __init__(self, name: str, cost: int, rarity: str,
+                 durability: int, effect: str):
+        super().__init__(name, cost, rarity)
+        self.durability = durability
+        self.effect = effect
+
+    def get_card_info(self):
+        infos: dict = super().get_card_info()
+        infos.update({'card_played': self.name,
+                      'mana_used': self.cost,
+                      'effect': self.effect,
+                      'type': 'artifacts'
+                      })
+        return infos
+
+    def play(self, game_state: dict) -> dict:
+        pass
+
+    def activate_ability(self) -> dict:
+        pass
--- ex1/SpellCard.py
+++ ex1/SpellCard.py
@@ -11,10 +11,10 @@ class SpellCard(Card):
     def get_card_info(self):
         infos: dict = super().get_card_info()
         infos.update({"effect_type": self.effect_type,
-                      "type": "spell"
+                      "type": "spells"
                       })
         return infos
-    
+
     def play(self, game_state: dict) -> dict:
        if game_state['mana'] < self.cost:
           raise ValueError("Not enough mana")
@@ -23,7 +23,6 @@ class SpellCard(Card):
                "mana_used": self.cost,
                "effect": self.effect_type
                }
-    
+
     def resolve_effect(self, targets: list) -> dict:
-       
-       
+       pass
